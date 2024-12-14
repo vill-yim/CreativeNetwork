dcf8586d72205506c7c956c7cc13f6a52f8e449e
yimco <yimco@mail.com>
2024-12-13 22:41:29 -0500

    listo calendary

--- src/components/home/components/Calendary.jsx
+++ src/components/home/components/Calendary.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "../../../styles/home/calendary.module.css";
+import {Calendly} from './Calendly'
+
 
 export const Calendary = () => {
   return (
@@ -11,6 +13,8 @@ export const Calendary = () => {
         <p>At the heart of everything we do is the unwavering belief in a future shaped by Bitcoin. A world where decentralization empowers individuals, sovereignty replaces control, and trust is no longer something to ask for but something embedded in the code. We see a future where innovation transcends barriers, where freedom flourishes, and where the financial system aligns with the principles of fairness and transparency.</p>
         <p>Our mission is to amplify the voices building this world. By empowering Bitcoin businesses with authentic, impactful marketing, we help them communicate their vision, grow their reach, and inspire the communities that believe in this movement. This isn’t just marketing. This is a movement. A fight for truth, for freedom, and for a future worth believing in. We’re here to ensure that the ethos of Bitcoin, the ethos of sovereignty and self-determination resonates in every corner of the world.</p>
       </div>
+
+      <Calendary />
     </div>
   );
 };
---
+++ src/components/home/components/Calendly.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { InlineWidget } from 'react-calendly';
+
+export const Calendly = () => {
+    return (
+        <InlineWidget url="https://calendly.com/creativenetwork" />
+    );
+};
